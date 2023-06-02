97e06979c36f281d09ac46d22b46a0bdfefa0149
Николай Батычко <nbngamings@gmail.com>
2023-06-02 14:17:37 +0300

    Настройка CORS

--- phoneaccounting/settings.py
+++ phoneaccounting/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'main.apps.MainConfig',
     # 'django.contrib.admin',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,3 +138,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'https://phoneaccounting.netlify.app'
+]
