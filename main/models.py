07f590504c2bb369442e95d4e65d13869464b838
Николай Батычко <nbngamings@gmail.com>
2023-06-02 15:37:55 +0300

    Подправил модели

--- main/models.py
+++ main/models.py
@@ -2,35 +2,35 @@ from django.db import models
 
 
 class Phone(models.Model):
-    manufacturer = models.CharField(max_length=255, default='')
-    model = models.CharField(max_length=255, default='')
-    os_version = models.CharField(max_length=255, default='')
-    converted_os_version = models.CharField(max_length=255, default='')
-    firmware = models.CharField(max_length=255, default='')
+    manufacturer = models.CharField(max_length=255, blank=True)
+    model = models.CharField(max_length=255, blank=True)
+    os_version = models.CharField(max_length=255, blank=True)
+    converted_os_version = models.CharField(max_length=255, blank=True)
+    firmware = models.CharField(max_length=255, blank=True)
     sd_slots_count = models.PositiveSmallIntegerField()
-    sdcard_serial_number = models.CharField(max_length=255, default='')
+    sdcard_serial_number = models.CharField(max_length=255, blank=True)
     sim_slots_count = models.PositiveSmallIntegerField()
-    simcard1 = models.CharField(max_length=255, default='')
-    simcard2 = models.CharField(max_length=255, default='')
-    supported_arch = models.CharField(max_length=255, default='')
-    user = models.CharField(max_length=255, default='')
+    simcard1 = models.CharField(max_length=255, blank=True)
+    simcard2 = models.CharField(max_length=255, blank=True)
+    supported_arch = models.CharField(max_length=255, blank=True)
+    user = models.CharField(max_length=255, blank=True)
 
 
 class SDCard(models.Model):
     in_phone = models.BooleanField(default=False)
-    name = models.CharField(max_length=255, default='')
-    serial_number = models.CharField(max_length=255, default='')
-    size = models.CharField(max_length=255, default='')
+    name = models.CharField(max_length=255, blank=True)
+    serial_number = models.CharField(max_length=255, blank=True)
+    size = models.CharField(max_length=255, blank=True)
 
 
 class SIMCard(models.Model):
     in_phone = models.BooleanField(default=False)
     locked = models.BooleanField(default=False)
-    number = models.CharField(max_length=255, default='')
-    operator_name = models.CharField(max_length=255, default='')
+    number = models.CharField(max_length=255, blank=True)
+    operator_name = models.CharField(max_length=255, blank=True)
 
 
 class Notification(models.Model):
-    text = models.CharField(max_length=255, default='')
-    package_name = models.CharField(max_length=255, default='')
-    title = models.CharField(max_length=255, default='')
+    text = models.CharField(max_length=255, blank=True)
+    package_name = models.CharField(max_length=255, blank=True)
+    title = models.CharField(max_length=255, blank=True)
